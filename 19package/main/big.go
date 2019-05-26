7b93d90284d9b2d695ee67b5dbf8d2eb00825dc0
Liao Bei <bliao@best-inc.com>
2019-05-26 14:36:12 +0800

    package

---
+++ 19package/main/big.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func main() {
+	im := big.NewInt(math.MaxInt64)
+	in := im
+	io := big.NewInt(1788)
+	ip := big.NewInt(1)
+
+	ip.Mul(im, in).Add(ip, im).Div(ip, io)
+	fmt.Printf("big int : %v", ip)
+
+	rm := big.NewRat(math.MaxInt64, 1956)
+	rn := big.NewRat(-1956, math.MaxInt64)
+	ro := big.NewRat(19, 56)
+	rp := big.NewRat(1111, 2222)
+	rq := big.NewRat(1, 1)
+	rq.Mul(rm, rn).Add(rq, ro).Mul(rq, rp)
+	fmt.Printf("big rat: %v\n", rq)
+}
---
+++ 19package/package_tes.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"./pack1"
+	"fmt"
+)
+
+func main() {
+	var test1 string
+	test1 = pack1.ReturnStr()
+	fmt.Print(test1)
+
+}
